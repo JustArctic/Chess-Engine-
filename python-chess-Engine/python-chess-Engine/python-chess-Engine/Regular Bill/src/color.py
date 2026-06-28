9319ab923a5d3e8fdb1eaf4fe164eb5788f0e21b
JustArctic <allan.luong@education.nsw.gov.au>
2026-06-28 15:57:18 +1000

    Enhance Color class constructor documentation
    Added detailed docstring for the Color class constructor, explaining parameters and usage.

--- python-chess-Engine/python-chess-Engine/python-chess-Engine/Regular Bill/src/color.py
+++ python-chess-Engine/python-chess-Engine/python-chess-Engine/Regular Bill/src/color.py
@@ -1,9 +1,24 @@
-# color.py
-# Part of the Bonnyrigg Chess Engine project.
-# Auto-documented with descriptive comments.
-
 class Color:
 
+    # ------------------------------------------------------
+    # Constructor
+    # ------------------------------------------------------
+    # Creates a colour theme consisting of a light colour
+    # and a dark colour.
+    #
+    # Parameters:
+    # light - RGB value for the light colour.
+    # dark  - RGB value for the dark colour.
+    #
+    # Used throughout the game to define:
+    # • Chess board colours
+    # • Highlight colours
+    # • Theme colour combinations
+    # ------------------------------------------------------
     def __init__(self, light, dark):
+
+        # Store the light colour.
         self.light = light
+
+        # Store the dark colour.
         self.dark = dark
